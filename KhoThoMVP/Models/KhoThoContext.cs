9bacca9289eab1082041bc12d886c68c8cf6f247
ngocdung1004 <nguyenngocdung100420032k3@gmail.com>
2024-11-03 18:51:21 +0700

    [update] Add password when Create new User [Update] update file context, only using dbcontext in appsetting.json

--- KhoThoMVP/Models/KhoThoContext.cs
+++ KhoThoMVP/Models/KhoThoContext.cs
@@ -33,8 +33,20 @@ public partial class KhoThoContext : DbContext
     public virtual DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-SVMK9VQ;Initial Catalog=dungnn_exe101_thodung5;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+    {
+        optionsBuilder.UseSqlServer(GetConnectionString());
+    }
+    private string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true, true)
+                    .Build();
+        var strConn = config["ConnectionStrings:DefaultConnection"];
+
+        return strConn;
+    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
