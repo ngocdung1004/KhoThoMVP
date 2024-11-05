8c9b8220e0e8aad0ad79c14cfd740dbc39a59d2c
ngocdung1004 <nguyenngocdung100420032k3@gmail.com>
2024-11-05 11:17:32 +0700

    [update] worker controller

---
+++ KhoThoMVP/DTOs/CreateWorkerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace KhoThoMVP.DTOs
+{
+    public class CreateWorkerDto
+    {
+        public int UserId { get; set; }
+        public int ExperienceYears { get; set; }
+        public float? Rating { get; set; }
+        public string? Bio { get; set; }
+        public bool? Verified { get; set; }
+    }
+}
--- KhoThoMVP/Interfaces/IWorkerService.cs
+++ KhoThoMVP/Interfaces/IWorkerService.cs
@@ -6,7 +6,7 @@ namespace KhoThoMVP.Interfaces
     {
         Task<IEnumerable<WorkerDto>> GetAllWorkersAsync();
         Task<WorkerDto> GetWorkerByIdAsync(int id);
-        Task<WorkerDto> CreateWorkerAsync(WorkerDto workerDto);
+        Task<WorkerDto> CreateWorkerAsync(CreateWorkerDto workerDto);
         Task<WorkerDto> UpdateWorkerAsync(int id, WorkerDto workerDto);
         Task DeleteWorkerAsync(int id);
         Task<IEnumerable<WorkerDto>> GetWorkersByJobTypeAsync(int jobTypeId);
