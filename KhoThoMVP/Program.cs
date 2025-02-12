bf96fc148a36f50b03e575f1211f5ac9e47a253b
ngocdung1004 <nguyenngocdung100420032k3@gmail.com>
2025-02-12 15:48:04 +0700

    [Update] appsetting.json

--- KhoThoMVP/Program.cs
+++ KhoThoMVP/Program.cs
@@ -98,7 +98,7 @@ namespace KhoThoMVP
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
